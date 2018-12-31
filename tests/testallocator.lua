cafb6b320b65c74d3468a4a1cf80bce25e927ae8
Pedro Tammela <pctammela@gmail.com>
2018-12-31 16:09:30 -0200

    adds allocator test

---
+++ tests/testallocator.lua
@@ -0,0 +1,12 @@
+--
+-- we are basically testing all possible calls to the allocator.
+-- this small code tests reallocations, frees and mallocs.
+--
+
+function process()
+   local tb = {}
+   for i = 1, 16777216 then
+      tb[i] = i
+   end
+   return false
+end
