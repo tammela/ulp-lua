30e25598d6077b705a5dd6b74c31c215169e8ef6
Pedro Tammela <pctammela@gmail.com>
2019-01-12 21:21:10 -0200

    add kmemfun test

---
+++ tests/kernel/kmemfun.c
@@ -0,0 +1,51 @@
+/**
+ * A module for memory allocator testing.
+ **/
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt "\n"
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/mm.h>
+#include <linux/slab.h>
+
+static void do_kmalloc(void)
+{
+   size_t sz;
+   void *ptr = NULL;
+   void *optr = NULL;
+
+   ptr = kmalloc(2, GFP_KERNEL); /* minimum allocation possible */
+   for (sz = 1; ptr != NULL; sz <<= 1) {
+      pr_err("=== reallocating %ld bytes ===", sz);
+      optr = ptr;
+      ptr = krealloc(ptr, sz, GFP_KERNEL);
+      if (optr != ptr && ptr != NULL)
+         pr_err("=== reallocated to new address with size %ld bytes ===",
+               ksize(ptr));
+      if (ptr)
+         pr_err("=== real size %ld bytes ===", ksize(ptr));
+      if (!ptr)
+         pr_err("=== maximum reallocation is %ld ===", (sz >> 1));
+   }
+}
+
+/**
+ * it is known that vmalloc() only allocates whole pages.
+ * more info here --> https://lwn.net/Articles/711653/
+ **/
+
+static int __init modinit(void)
+{
+   pr_err("=== page size %ld bytes ===", PAGE_SIZE);
+   pr_err("=== kmalloc ===");
+   do_kmalloc();
+   return 0;
+}
+
+static void __exit modexit(void)
+{
+}
+
+module_init(modinit);
+module_exit(modexit);
+MODULE_AUTHOR("Pedro Tammela <pctammela@gmail.com>");
+MODULE_LICENSE("GPL");
