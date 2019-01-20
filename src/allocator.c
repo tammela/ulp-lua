ce93c5e245e0283b2119ed50fb62996c2bc83778
Pedro Tammela <pctammela@gmail.com>
2019-01-20 15:07:40 -0200

    optimize virtual reallocations
    also supresses an annoying warning

--- src/allocator.c
+++ src/allocator.c
@@ -1,12 +1,30 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt "\n"
 #include <linux/kernel.h>
+#include <linux/slab.h>
 #include <linux/mm.h>
 
-#include <lua.h>
+/*
+ * check if the virtual reallocation fits the current allocation.
+ * vmalloc() minimum allocation size is a page.
+ * make sure the reallocation is really needed.
+ */
+static void *checksize(void *ptr, size_t osize, size_t nsize)
+{
+   if (nsize > osize) /* enlarging */
+      return PAGE_ALIGN(nsize) > PAGE_ALIGN(osize) ? NULL : ptr;
+   else /* shrinking or spurious */
+      return ptr;
+}
 
 static void *vrealloc(void *ptr, size_t osize, size_t nsize)
 {
    void *nptr;
+
+   if (is_vmalloc_addr(ptr)) {
+      nptr = checksize(ptr, osize, nsize);
+      if (nptr)
+         return nptr;
+   }
 
    nptr = vmalloc(nsize);
 
@@ -21,8 +39,8 @@ static void *vrealloc(void *ptr, size_t osize, size_t nsize)
 
 void *allocator(void *ud, void *ptr, size_t osize, size_t nsize)
 {
-   (void)(ud);
    void *nptr;
+   (void)(ud);
 
    if (nsize == 0) {
       kvfree(ptr);
