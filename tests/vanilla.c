a2c989ea188b2a2f00a89620a49ab4b2c67e6661
Pedro Tammela <pctammela@gmail.com>
2019-02-09 11:55:18 -0200

    rename test servers file

--- tests/vanilla.c
+++ tests/vanilla.c
@@ -13,9 +13,9 @@
 #include <linux/in.h>
 #include <linux/tcp.h>
 
-#include <lua5.3/lua.h>
-#include <lua5.3/lauxlib.h>
-#include <lua5.3/lualib.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
 
 #include <luadata.h>
 
