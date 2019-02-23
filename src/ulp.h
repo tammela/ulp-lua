c4fa476228052ddb1b94433c29ab839e4399488d
Pedro Tammela <pctammela@gmail.com>
2019-02-23 14:57:57 -0300

    detail on ulp.h

--- src/ulp.h
+++ src/ulp.h
@@ -11,6 +11,31 @@
 #include "pool.h"
 #include "conf.h"
 
+/* FROM: NFLua */
+/*
+ * Copyright (C) 2017-2018 CUJO LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+#define luaU_setenv(L, env, st) { \
+	st **penv = (st **)lua_getextraspace(L); \
+	*penv = env; }
+
+#define luaU_getenv(L, st) (*((st **)lua_getextraspace(L)))
+/* END */
+
 /* We must define SOL_LUA in /include/linux/socket.h */
 #define SOL_LUA                (999)
 
@@ -34,32 +59,7 @@ static inline struct context *sk_ulp_ctx(struct sock *sk)
    if (!L)
       return NULL;
 
-   return *(struct context **)lua_getextraspace(L);
+   return luaU_getenv(L, struct context);
 }
-
-/* Code below taken from NFLua */
-
-/*
- * Copyright (C) 2017-2018 CUJO LLC
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- */
-#define luaU_setenv(L, env, st) { \
-	st **penv = (st **)lua_getextraspace(L); \
-	*penv = env; }
-
-#define luaU_getenv(L, st) (*((st **)lua_getextraspace(L)))
 
 #endif
