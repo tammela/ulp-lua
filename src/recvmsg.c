b527f28526bbb9aca40411f8d8a1eb90f1db5516
Pedro Tammela <pctammela@gmail.com>
2019-02-23 14:58:46 -0300

    wrap Lua code around pcall and bail out on more cases
    fixes #21 and #27.

--- src/recvmsg.c
+++ src/recvmsg.c
@@ -16,15 +16,34 @@
 #include "syscalls.h"
 #include "pretty.h"
 
+int __doprocess(lua_State *L)
+{
+   struct sk_buff *skb = lua_touserdata(L, 1);
+   struct context *ctx = luaU_getenv(L, struct context);
+   int perr = 0;
+   int data = LUA_NOREF;
+
+   if (lua_getglobal(L, ctx->entry) != LUA_TFUNCTION)
+      return luaL_error(L, "couldn't find context function: %s\n", ctx->entry);
+
+   data = ldata_newref(L, skb->data, skb->len);
+   perr = lua_pcall(L, 1, 1, 0);
+   ldata_unref(L, data);
+   if (perr)
+      return lua_error(L);
+
+   return 0;
+}
+
 int ulp_recvmsg(struct sock *sk, struct msghdr *msg, size_t len,
       int nonblock, int flags, int *addr_len)
 {
+   struct tcp_sock *tp = tcp_sk(sk);
    lua_State *L = sk_ulp_data(sk)->L;
    struct context *ctx = sk_ulp_ctx(sk);
-   int perr = 0;
-   int data = LUA_NOREF;
    struct sk_buff *skb;
    struct tcphdr *hdr;
+   int perr;
 
    if (unlikely(sk->sk_state != TCP_ESTABLISHED))
       return -ENOTCONN;
@@ -39,32 +58,33 @@ int ulp_recvmsg(struct sock *sk, struct msghdr *msg, size_t len,
     * we check for flags that are not of our interest and bail out
     * if we find any.
     */
-
-   if (unlikely(flags & MSG_ERRQUEUE))
+   if (unlikely(flags & MSG_ERRQUEUE) || flags & MSG_OOB || flags & MSG_PEEK)
       goto out;
 
-   if (flags & MSG_OOB)
+   /* special case on Linux.
+    * https://lwn.net/Articles/495304/
+    */
+   if (unlikely(tp->repair))
       goto out;
 
-   if (flags & MSG_PEEK)
+   /* we bail out until the userspace process reads the urgent data */
+   if (tp->urg_data)
       goto out;
 
    if (skb_queue_empty(&sk->sk_receive_queue) || !ctx->entry[0])
       goto out;
 
-   if (lua_getglobal(L, ctx->entry) != LUA_TFUNCTION) {
-      lua_pop(L, 1);
-      goto out;
-   }
-
    skb = skb_peek_tail(&sk->sk_receive_queue);
-   hdr = tcp_hdr(skb);
 
    /* while we don't have sparse buffer pattern matching in Lua,
     * we have to drop non-linears skbs processing.
     */
-   if (skb_is_nonlinear(skb))
+   if (skb_is_nonlinear(skb)) {
+      pp_warn("non linear processing is not implemented");
       goto out;
+   }
+
+   hdr = tcp_hdr(skb);
 
    /* this is an empiric assertion, as data may be received via ACKs as well.
     * right now we are considering only GRO-on cases, where a typical HTTP
@@ -73,10 +93,12 @@ int ulp_recvmsg(struct sock *sk, struct msghdr *msg, size_t len,
    if (unlikely(!hdr->psh))
       goto out;
 
-   data = ldata_newref(L, skb->data, skb->len);
+   /* Lua guarantees some stack space */
+   lua_pushlightuserdata(L, (void *) skb);
+   lua_pushcfunction(L, __doprocess);
+
    perr = lua_pcall(L, 1, 1, 0);
-   ldata_unref(L, data);
-   if (unlikely(perr)) {
+   if (perr) {
       pp_pcall(perr, lua_tostring(L, -1));
       lua_pop(L, 1);
       goto out;
