803dd78a3b3b8ddc4ce6c05fdbca40567eb75af5
Pedro Tammela <pctammela@gmail.com>
2019-03-31 21:09:20 +0000

    update insmod script

--- quick-insmod.sh
+++ quick-insmod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo insmod ./dependencies/lunatik-hidden/lunatik.ko
+sudo insmod ./dependencies/lunatik/lunatik.ko
 sudo insmod ./dependencies/luadata/luadata.ko
 sudo insmod ./src/ulp-lua.ko
