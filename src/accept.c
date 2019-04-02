68057f1a49051a0015bfab7f9604c8f182e8f5eb
pedro tammela <pctammela@cujo.com>
2019-04-02 20:21:42 -0300

    fixup! integrate with tls

--- src/accept.c
+++ src/accept.c
@@ -14,6 +14,9 @@ struct sock *ulp_accept(struct sock *sk, int flags, int *err, bool kern)
 {
    int ret = 0;
    struct sock *reqsk = sys->accept(sk, flags, err, kern);
+#ifdef HAS_TLS
+   void *ops;
+#endif
 
    if (reqsk == NULL)
       return NULL;
@@ -30,11 +33,15 @@ struct sock *ulp_accept(struct sock *sk, int flags, int *err, bool kern)
    }
 
 #ifdef HAS_TLS
+   ops = inet_csk(reqsk)->icsk_ulp_ops;
+   /* fake a clean ulp socket */
+   inet_csk(reqsk)->icsk_ulp_ops = NULL;
    ret = ((int (*)(struct sock *, const char *))addr_tcp_set_ulp)(reqsk, "tls");
    if (ret) {
       pp_errno(ret, "caught errno in TLS ulp");
       *err = ret;
    }
+   inet_csk(reqsk)->icsk_ulp_ops = ops;
 #endif
 
    inet_csk(reqsk)->icsk_ulp_data = pool_pop(inet_csk(reqsk)->icsk_ulp_data);
